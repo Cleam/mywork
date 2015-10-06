409e58a8298e01d82421553ada3f4f017290c645
lizhigao <lizhigao1231@163.com>
2015-10-06 12:10:54 +0800

    一些小的修改

--- h5_imgView/js/page.js
+++ h5_imgView/js/page.js
@@ -1,7 +1,7 @@
 (function(){
 	window.onload=function(){
 		var imgs = document.querySelectorAll('img');
-		var defultTransform = null;
+		// var defultTransform = null;
 		for (var i = 0; i < imgs.length; i++) {
 			var ranNum1 = Math.floor(100 * Math.random() - 50); // 获取-50~50之间的随机数
 			imgs[i].style.transform = 'rotate(' + ranNum1 + 'deg)';
@@ -9,11 +9,13 @@
 			imgs[i].onmouseout = mout;
 		}
 		function mover(e){
-			defultTransform = this.style.transform;
+			// defultTransform = this.style.transform;
 			this.style.transform = 'rotate(0deg) scale(1.2,1.2)';
 		}
 		function mout(){
-			this.style.transform = defultTransform;
+			// this.style.transform = defultTransform;
+			var ranNum = Math.floor(100 * Math.random() - 50); // 获取-50~50之间的随机数
+			this.style.transform = 'rotate(' + ranNum + 'deg)';
 		}
 	};
 })();
