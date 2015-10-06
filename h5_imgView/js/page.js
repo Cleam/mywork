0f081558213d9711741ded3eacc9d2f6faf59d84
lizhigao <lizhigao1231@163.com>
2015-10-06 12:02:34 +0800

    完成照片墙简单示例

---
+++ h5_imgView/js/page.js
@@ -0,0 +1,19 @@
+(function(){
+	window.onload=function(){
+		var imgs = document.querySelectorAll('img');
+		var defultTransform = null;
+		for (var i = 0; i < imgs.length; i++) {
+			var ranNum1 = Math.floor(100 * Math.random() - 50); // 获取-50~50之间的随机数
+			imgs[i].style.transform = 'rotate(' + ranNum1 + 'deg)';
+			imgs[i].onmouseover = mover;
+			imgs[i].onmouseout = mout;
+		}
+		function mover(e){
+			defultTransform = this.style.transform;
+			this.style.transform = 'rotate(0deg) scale(1.2,1.2)';
+		}
+		function mout(){
+			this.style.transform = defultTransform;
+		}
+	};
+})();
